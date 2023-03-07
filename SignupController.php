feb4f048d725c03bbcdaabb20e3d14f7fb709cd0
KD2029 <109062974+KD2029@users.noreply.github.com>
2023-03-07 20:59:34 +0300

    this includes the code for the bmi calculator

---
+++ SignupController.php
@@ -0,0 +1,55 @@
+<?php
+ 
+
+namespace App\Http\Controllers;
+require '../vendor/autoload.php';
+
+use Illuminate\Http\Request;
+
+class SignupController extends Controller
+{
+    //
+    public function index(){
+      
+      return view('Signup');
+      
+  }
+
+  public function post (Request $request){
+      
+    $validated = $request ->validate([
+      'mass' => 'required|min:2',
+      'height' => 'required|min:1']);
+    
+     //print_r($validated);
+    // $data = $request->input('ingr');
+    $mass = $validated['mass'];
+    $height = $validated['height'];
+
+      $bmi = $mass/$height;
+      
+     if ($bmi <= 18.5) {
+      $output = "UNDERWEIGHT";
+      $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
+      return view('Signup')->with('k',$result);
+      } else if ($bmi > 18.5 AND $bmi<=24.9 ) {
+      $output = "NORMAL WEIGHT";
+      $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
+      return view('Signup')->with('k',$result);
+      } else if ($bmi > 24.9 AND $bmi<=29.9) {
+      $output = "OVERWEIGHT";
+      $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
+      return view('Signup')->with('k',$result);
+      } else if ($bmi > 30.0) {
+      $output = "OBESE";
+      $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
+      return view('Signup')->with('k',$result);
+  }
+   
+  
+  
+}
+
+
+  
+}
