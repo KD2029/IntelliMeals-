bd35c9b13eaf5ddc0e75fe59acbf8fb6e5dea619
Onen Sensy <sensyonen@gmail.com>
2023-03-15 08:16:31 +0300

    FIX: BMI code --Added routes to handle bmi -- Cleaned the code -- Prettified code

--- app/Http/Controllers/bmicontroller.php
+++ app/Http/Controllers/bmicontroller.php
@@ -7,39 +7,43 @@ use Illuminate\Http\Request;
 class bmicontroller extends Controller
 {
     //
-    public function index() {
+    public function index()
+    {
         return view("bmi");
     }
-    public function post (Request $request){
-      
-        $validated = $request ->validate([
-          'mass' => 'required|min:2',
-          'height' => 'required|min:1']);
-        
-         //print_r($validated);
+
+    // Post is not an appropriate name to give to such a function. Te name should state what the function does
+    public function getBMI(Request $request)
+    {
+
+        $validated = $request->validate([
+            'mass' => 'required|min:2',
+            'height' => 'required|min:1'
+        ]);
+
+        //print_r($validated);
         // $data = $request->input('ingr');
         $mass = $validated['mass'];
         $height = $validated['height'];
-    
-          $bmi = $mass/$height;
-          
-         if ($bmi <= 18.5) {
-          $output = "UNDERWEIGHT";
-          $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
-          return view('bmi')->with('k',$result);
-          } else if ($bmi > 18.5 AND $bmi<=24.9 ) {
-          $output = "NORMAL WEIGHT";
-          $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
-          return view('bmi')->with('k',$result);
-          } else if ($bmi > 24.9 AND $bmi<=29.9) {
-          $output = "OVERWEIGHT";
-          $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
-          return view('bmi')->with('k',$result);
-          } else if ($bmi > 30.0) {
-          $output = "OBESE";
-          $result = "Your BMI value is  " . $bmi . "  and you are : " .$output; 
-          return view('bmi')->with('k',$result);
-      }
-      
-}
+
+        $bmi = $mass / $height;
+
+        if ($bmi <= 18.5) {
+            $output = "UNDERWEIGHT";
+            $result = "Your BMI value is  " . $bmi . "  and you are : " . $output;
+            return view('bmi')->with('k', $result);
+        } else if ($bmi > 18.5 and $bmi <= 24.9) {
+            $output = "NORMAL WEIGHT";
+            $result = "Your BMI value is  " . $bmi . "  and you are : " . $output;
+            return view('bmi')->with('k', $result);
+        } else if ($bmi > 24.9 and $bmi <= 29.9) {
+            $output = "OVERWEIGHT";
+            $result = "Your BMI value is  " . $bmi . "  and you are : " . $output;
+            return view('bmi')->with('k', $result);
+        } else if ($bmi > 30.0) {
+            $output = "OBESE";
+            $result = "Your BMI value is  " . $bmi . "  and you are : " . $output;
+            return view('bmi')->with('k', $result);
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\bmicontroller;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\nutritionanalysiscontroller;
 /*
@@ -17,17 +18,21 @@ use App\Http\Controllers\nutritionanalysiscontroller;
 */
 
 Route::get('/', function () {
-    session()->put("LOGGED_IN",true);
+    session()->put("LOGGED_IN", true);
     return view('welcome');
 });
 //Route::get('/login',[AuthController::class,'index']);
-Route::get('/Logout',[LogoutController::class,'index']);
-Route::get('/login',[nutritionanalysiscontroller::class,'index']);
-Route::group(["middleware"=>"mygroup"],function(){
-Route::view("/users",'users');
+Route::get('/Logout', [LogoutController::class, 'index']);
+Route::get('/login', [nutritionanalysiscontroller::class, 'index']);
+Route::group(["middleware" => "mygroup"], function () {
+    Route::view("/users", 'users');
 
-Route::view("/dashboard",'dashboard');
-Route::get('/Logout',[LogoutController::class,'index']);
-Route::get('/dashboard',[nutritionanalysiscontroller::class,'index']);
-Route::post('/nutritionanalysis',[nutritionanalysiscontroller::class,'inserter']);
+    Route::view("/dashboard", 'dashboard');
+    Route::get('/Logout', [LogoutController::class, 'index']);
+    Route::get('/dashboard', [nutritionanalysiscontroller::class, 'index']);
+    Route::post('/nutritionanalysis', [nutritionanalysiscontroller::class, 'inserter']);
 });
+
+//Below are routes i added for the bmi
+Route::get('/bmi', [bmicontroller::class, 'index']); //This displays the form
+Route::post('/bmi', [bmicontroller::class, 'getBmi']); //This handles the submission
